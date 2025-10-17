18dabc9a74f389f91ab4035d1f4c690f946836d1
RANIA <rania.zhirii@gmail.com>
2025-10-17 23:50:01 +0100

    TP3

---
+++ TP3/src/main/java/metier/MetierImpl.java
@@ -0,0 +1,22 @@
+package metier;
+
+import dao.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("metier")
+public class MetierImpl implements IMetier {
+    @Autowired
+    private IDao dao;                 // pas de @Qualifier ici
+    @Override
+    public double calcul() { return dao.getValue() * 2; }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("[TRACE] DAO injecté = " + dao.getClass().getSimpleName());
+    }
+
+
+    public void setDao(IDao dao) {
+    }
+}
