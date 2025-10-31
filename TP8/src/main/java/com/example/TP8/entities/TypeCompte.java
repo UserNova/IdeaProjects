fef9c33a8afc008cef08d2c8b48f269de078735c
RANIA <rania.zhirii@gmail.com>
2025-10-31 01:31:25 +0100

    Ajout du tp8

---
+++ TP8/src/main/java/com/example/TP8/entities/TypeCompte.java
@@ -0,0 +1,5 @@
+package com.example.TP8.entities;
+
+public enum TypeCompte {
+    COURANT, EPARGNE
+}
---
+++ TP8/src/main/java/com/example/TP8/repositories/CompteRepository.java
@@ -0,0 +1,9 @@
+package com.example.TP8.repositories;
+
+import com.example.TP8.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+}
